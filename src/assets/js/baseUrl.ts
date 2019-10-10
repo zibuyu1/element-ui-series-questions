edcb7b10fec056c3e4169ccb2532aed20ba8197c
zibuyu <1748500626@qq.com>
2019-10-10 14:09:22 +0800

    Update baseUrl.ts

--- src/assets/js/baseUrl.ts
+++ src/assets/js/baseUrl.ts
@@ -1,13 +1,10 @@
 let baseUrl;
 switch (location.hostname) {
   case 'localhost':
-    baseUrl = 'https://test_iq.haizol.com/api';
+    baseUrl = 'https://xxxxx/api';
     break;
   case 'test_iq.haizol.com':
-    baseUrl = 'https://test_iq.haizol.com/api';
-    break;
-  case 'iq.haizol.com':
-    baseUrl = 'https://iq.haizol.com/api';
+    baseUrl = 'https://xxxxx/api';
     break;
 }
 const urls = {
